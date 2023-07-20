297fef66152760b48f4107858b4e71a986588127
Adeniyi Emmanuel <adeniyiemmanuel998@gmail.com>
2023-07-20 15:33:08 -0700

    back1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+	/**
+	 * main - function entry point
+	 * Return: 0 to show code executed successfully
+	 */
+
+	int main(void)
+
+	{
+	puts("\"Programming is like building a multili gual puzzle");
+	return (0);
+	}
