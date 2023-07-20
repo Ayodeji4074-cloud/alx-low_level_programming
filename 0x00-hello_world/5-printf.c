de86ec78608387a5b26091f3d26c7a94fcf5c801
Adeniyi Emmanuel <adeniyiemmanuel998@gmail.com>
2023-07-20 16:26:24 -0700

    back6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- Function entry
+ * Return- 0 (as printed)
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+	return (0);
+{
