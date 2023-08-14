f90ede172c8867395aa2520b4f974184bb7bc057
Adeniyi Emmanuel <adeniyiemmanuel998@gmail.com>
2023-08-14 15:54:50 -0700

    pre4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - main function for a file name
  * Returns: 0 for successful operation
  */
 
-int main(void) {
+int main(void)
+{
 
 	printf("File: %s\n", _FILE_);
 	
