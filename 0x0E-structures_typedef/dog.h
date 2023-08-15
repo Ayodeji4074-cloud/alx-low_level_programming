c18783220654ba2ece38c43eaae1523caffe2fd9
Adeniyi Emmanuel <adeniyiemmanuel998@gmail.com>
2023-08-15 15:47:44 -0700

    strt2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,21 @@
-#ifndef dog
-#define struct dog {
+#ifndef DOG
+
+#define DOG
+
+/**
+ * struct: struct for dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
 	char name[];
 	float age;
-	char owner;
+	char owner[];
 
-}
+};
 
 #endif
 
