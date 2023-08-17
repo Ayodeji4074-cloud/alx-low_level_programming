b845b21c96b95815e4e628d0f18c8cf90eec68f9
Adeniyi Emmanuel <adeniyiemmanuel998@gmail.com>
2023-08-17 05:06:53 -0700

    pnt1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - it prints a name
+ * @name: Name to print
+ * @f: A pointer that points to a function printing a name
+ */
+
+char print_name(char);
+
+void main()
+{
+	char name;
+	char(*f)=&print_name
+}
+
+if(f==NULL)
+	return (f(name));
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
+
+
+
+
+
+#endif
