0c0f82120bc099ad785b76a3fa3f2d7eb5c6ec4a
Adeniyi Emmanuel <adeniyiemmanuel998@gmail.com>
2023-08-28 13:57:13 -0700

    one single

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all the elements of a list.
+ * @h: head pointer of a list.
+ *
+ * Return: Returns numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i  = 0;
+
+	while (h != 0)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (n);
+}
